bd06541862de98d277267cfd3e6323ea3290f381
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2024-08-04 22:44:03 +0200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 // Routes
 const passwordRoutes = require('./routes/passwordRoutes');
 const colorPaletteRoutes = require('./routes/colorPaletteRoutes');
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Middleware
 app.use('/password', passwordRoutes);
