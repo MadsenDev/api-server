dec68d3ccdc9b5bff819e325afe1e55cf9782d6b
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2024-08-05 22:01:45 +0200

    Added dinner suggestions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 // Routes
 const passwordRoutes = require('./routes/passwordRoutes');
 const colorPaletteRoutes = require('./routes/colorPaletteRoutes');
+const dinnerRoutes = require('./routes/dinnerRoutes');
 
 // Enable CORS for all routes
 app.use(cors());
@@ -13,6 +14,7 @@ app.use(cors());
 // Middleware
 app.use('/password', passwordRoutes);
 app.use('/color-palette', colorPaletteRoutes);
+app.use('/dinner-suggestion', dinnerRoutes);
 
 app.listen(port, () => {
   console.log(`API server is running at http://localhost:${port}`);
